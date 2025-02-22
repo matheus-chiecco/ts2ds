7db8936cccb23a1334ac0985a9f557d4d7735310
matheus-chiecco <matheuschiecco12@gmail.com>
2025-02-21 22:31:58 -0300

    inserindo elementos de formulário

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 "use strict";
 var empresa;
 (function (empresa) {
-    let p = new empresa.pessoa();
-    p.nome = "Geovana";
-    p.anoNasc = 2008;
-    document.getElementById("nome").textContent = p.nome;
-    document.getElementById("ano").textContent = p.anoNasc.toString();
-    document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    const calc = document.getElementById("calc");
+    const campoNome = document.getElementById("campoNome");
+    const campoAno = document.getElementById("campoAno");
+    calc.addEventListener("click", () => {
+        let p = new empresa.pessoa();
+        p.nome = campoNome.value;
+        p.anoNasc = parseInt(campoAno.value);
+        document.getElementById("nome").textContent = p.nome;
+        document.getElementById("ano").textContent = p.anoNasc.toString();
+        document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    });
 })(empresa || (empresa = {}));
--- ts/main.ts
+++ ts/main.ts
@@ -1,9 +1,17 @@
 namespace empresa{
+const calc = document.getElementById("calc") as HTMLButtonElement;
+const campoNome = document.getElementById("campoNome") as HTMLInputElement;
+const campoAno = document.getElementById("campoAno") as HTMLInputElement;
+
+calc.addEventListener("click", ()=>{
     let p = new pessoa();
-    p.nome = "Geovana";
-    p.anoNasc = 2008;
+    p.nome = campoNome.value;
+    p.anoNasc = parseInt(campoAno.value);
 
     document.getElementById("nome").textContent = p.nome;
     document.getElementById("ano").textContent = p.anoNasc.toString();
     document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    
+    })
+    
 }
